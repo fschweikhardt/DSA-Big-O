7c1220f6c815508f267c747f6e94bf4cb953d5e0
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-07 23:19:01 -0500

    Update bigO.js

--- bigO.js
+++ bigO.js
@@ -231,3 +231,13 @@ countTriangle(16);
             }
         }
     }
+
+'6. Creating pairs'
+    function createPairs(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            for(let j = i + 1; j < arr.length; j++) {
+                console.log(arr[i] + ", " +  arr[j] );
+            }
+        }
+    }
+
