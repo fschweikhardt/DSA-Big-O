493f3cfc07e38c91544893df7e4c26eb4ad6de95
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-07 23:19:43 -0500

    Update bigO.js

--- bigO.js
+++ bigO.js
@@ -241,3 +241,22 @@ countTriangle(16);
         }
     }
 
+'7. Compute the sequence'
+    function compute(num) {
+        let result = [];
+        for (let i = 1; i <= num; i++) {
+
+            if (i === 1) {
+                result.push(0);
+            }
+            else if (i === 2) {
+                result.push(1);
+            }
+            else {
+                result.push(result[i - 2] + result[i - 3]);
+            }
+        }
+        return result;
+    }
+
+
