11b5c244a72b8415f69c8a7b56fb474c5eb54219
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-08 22:18:33 -0500

    Update iterative-version.js

--- iterative-version.js
+++ iterative-version.js
@@ -1,6 +1,15 @@
 'iterative version of recursive algorithms'
 
+'1. Counting Sheep'
+
+const sheep = num => {
+    for (let i = num; i > 0; i--) {
+        console.log(i, ': Another sheep!')
+    }
+    return console.log('All the sheep jumped!')
+}
+
+console.log(sheep(3))
 
 
-
-
+'2. Power Calculator'
