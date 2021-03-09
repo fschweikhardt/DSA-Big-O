99b5ace6a08d34be299508d92df8eb143caf6b25
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-08 22:55:36 -0500

    Update iterative-version.js

--- iterative-version.js
+++ iterative-version.js
@@ -13,3 +13,18 @@ console.log(sheep(3))
 
 
 '2. Power Calculator'
+
+let calculator = (number, exponent) => {
+    let answer = 1;
+    if (exponent < 0) {
+        return 'exponent should be >= 0'
+    }
+    for (let i = 0; i < exponent; i++) {
+        answer *= number
+    }
+    return answer
+}
+
+console.log(calculator(10,3))
+console.log(calculator(3,3))
+console.log(calculator(10,-2))
