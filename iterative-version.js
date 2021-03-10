6ed3dee12ee82e10ece3ea0f4d675c7cea2883a5
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-10 11:49:14 -0500

    Update iterative-version.js

--- iterative-version.js
+++ iterative-version.js
@@ -28,3 +28,53 @@ let calculator = (number, exponent) => {
 console.log(calculator(10,3))
 console.log(calculator(3,3))
 console.log(calculator(10,-2))
+
+
+'3. Reverse String'
+
+const reverse = string => {
+    let flipped = ''
+    for (let i = string.length - 1; i >= 0; i--) {
+        flipped += string.slice(i, i + 1)
+    }
+    return flipped
+}
+
+console.log(reverse('jim'))
+
+
+'4. nth Triangular Number'
+
+const triangle = num => {
+    let answer = num
+    if (num === 1) {
+        return 1
+    }
+    for (let i = 1; i < num; i++) {
+        answer += i
+    }
+    return answer
+}
+
+console.log(triangle(5))
+
+
+'5. String Splitter'
+
+const splitter = (string, seperator) => {
+    let arr = [];
+    while(true) {
+        let idx = string.indexOf(seperator)
+        if (idx < 0) {
+            break;
+        }
+        arr.push(string.slice(0, idx))
+        string = string.slice(idx + 1)
+    }
+    arr.push(string)
+    return arr
+}
+
+console.log(splitter('02/02/3030', '/'))
+
+
